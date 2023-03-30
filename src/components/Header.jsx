de4fd92ffa33ec1ddecaff064c6dd71e92643876
Mohamed Aly <mohamedalyahmed@aol.com>
2023-03-30 02:39:08 +0200

    Build App Structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark text-center ">
+      <h1 className="text-white w-100">Book Store with (Redux ToolKit)</h1>
+    </nav>
+  );
+};
+
+export default Header;
